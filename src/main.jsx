a8fc525b4324d1b625160621a398f8b720f9ce4a
scm-minaungpaing <min.aungpaing@seattleconsultingmyanmar.com>
2023-02-16 02:47:11 +0630

    add: menu design and logout

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,14 @@ import {
 } from "react-router-dom";
 
 import router from './router/index'
+import { StateProvider } from './context/StateProvider'
+import { initialState } from './context/initalState'
+import reducer from './context/reducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <RouterProvider router={router} />
+    </StateProvider>
   </React.StrictMode>,
 )
